20531d7ef518555972c3ef02075038e47e119db6
Jack McNicol <jackmcpickle@gmail.com>
2021-11-02 10:24:39 +1030

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
+  bufferCommands: false, // Disable mongoose buffering
+  bufferMaxEntries: 0, // and MongoDB driver buffering
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
